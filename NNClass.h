75fc59107ffa5f6fb165c282836436340894b0e7
bablet <frk.haha@hotmail.se>
2018-03-20 20:50:32 +0100

    save/load

--- NNClass.h
+++ NNClass.h
@@ -7,24 +7,16 @@
 #include <iostream>
 #include <fstream>
 
-//NNClass:
-//  Feedforward Neural Network with dynamic sizing.
-//  Able to hande dynamic size of input. 
-//  Multiple inputs needs to be in same size.
-//  Can only handle input in vector form.
-//  Free sizing of hidden layers and output.
 class NNClass
 {
-	public:
-		NNClass(int data_size, std::vector<int> &layer_size); //Create a Neural Network with specified size
-		NNClass(int data_size, std::string &filename);
+	public:		//  data_size = amount of training data
+		NNClass(int data_size, std::vector<int> &layer_size); //Create new network with specified size.
+		NNClass(int data_size, std::string &filename); // Load weights from file
 		void train(std::vector<std::vector<float> > &input, std::vector<std::vector<float> > &target, float constant, int epochs);
-		void save(std::string filename);      // Save Neural Network
+		void save(std::string filename);      // Save weights
 		void destroy(){delete this;};
 	private:
-		//Layer Data, Keeps the information of a single layer.
-		//A layer is with the weights as input to a neuron and its output.
-		//Also keeps the information of the delta value for the neurons in the layer.
+		//Layer data
 		struct Layer_struct
 		{
 			std::vector<float> delta;
@@ -38,17 +30,17 @@ class NNClass
 
 		float constant; // Learning rate
 		int depth;    	// Depth of current Neural Network
-		int input_size; // Size of the given input
+		int input_size;
 		int data_size;
 		int epoch;
 	
 		//Setup functions
-		bool allocate_layers();   // Allocates the layer struct
-		bool randomize_weights(); // Randomizes weights
+		bool allocate_layers();
+		bool randomize_weights();
 
 		//Alghoritms
 		float cost();
 		bool backpropagation(int data);   
-		float activation(int layer, int index, int data);    // Needs to be specified.
+		float activation(int layer, int index, int data);
 };
 #endif
